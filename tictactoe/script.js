212d6a8707ab411362289a6e1016c015931be762
Jeff Bailey <northwestjeff@gmail.com>
2018-01-05 12:59:11 -0800

    init commit tictactoe

---
+++ tictactoe/script.js
@@ -0,0 +1,22 @@
+// Set up HTMl CSS JS Git
+// store gameboard inside array of gameboard object
+// players stored in objects
+// gameplay controller stored in objects
+
+// if you need ONE: module.  TWO: Factories
+
+// JS should render gameboard contents
+
+//build functions that allow players to add marks
+// no selecting spots already filled
+
+// Each piece of functionality should fit into game, player or gameboard
+
+// check for 3 in a row or tie
+
+// Clean up interface to add name, restart
+
+
+function gameBoard(cols, row) {
+    for (i=0; i<cols)
+}
